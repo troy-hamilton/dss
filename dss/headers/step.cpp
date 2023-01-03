27e53016f1b04e2cf84111fc7079b941531181cf
troy-hamilton <121830956+troy-hamilton@users.noreply.github.com>
2023-01-02 20:05:55 -0600

    Add files via upload

--- dss/headers/step.cpp
+++ dss/headers/step.cpp
@@ -12,5 +12,11 @@ void step(int client)
     
     printf("%s\n", (char*)raw.start);
     
+    const char*
+        reply = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello";
+        
+    retval = write(client, reply, strlen(reply));
     
+    cleanup(raw);
+    cleanup(client);
 }
